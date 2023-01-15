175310eb0c98acf39a4d381464a874e0aabd83cb
nelsondev <sandoval19371@gmail.com>
2023-01-15 02:21:00 -0300

    first commit

--- src/pasajeros/page/minaPage.jsx
+++ src/pasajeros/page/minaPage.jsx
@@ -1,6 +1,8 @@
 import React, {useState,useEffect} from 'react'
 import axios from 'axios'
-import * as xlsx from "xlsx"
+import xlsx from "json-as-xlsx"
+
+
 export const MinaPage = () => {
   const [users, setUsers] = useState ([])
   const [search, setSearch] = useState("")
@@ -31,9 +33,58 @@ export const MinaPage = () => {
   const searcher = (e) =>{
     setSearch(e.target.value)
   }
+  const results = !search ? users : users.filter((val)=>val.fecha.toLowerCase().includes(search.toLocaleLowerCase()))
+
+
+//Funcion para convertir a Excel
+
+  const converted =() =>{
+   
+     let result2 = results;
+      let data = [
+        {
+          sheet: "PASAJEROS",
+          columns: [
+            { label: "RUT", value: "user" }, // Top level data
+            { label: "FECHA", value: (row) => row.age  }, // Custom format
+            { label: "UBICACIÓN", value: (row) => (row.more ? row.more.phone || "" : "") }, // Run functions
+          ],
+          
+          content: [
+
+           
+
+           
+           
+
+            
+          ],
+        },
+        
+      ]
+
+     
+
+      for (let i = 0; i < result2.length; i++) {
+      //  data[1].content.push({ user: result2[0].name, age: result2[0].fecha, more: { phone: result2[0].ubicacion } })
+       data[0].content.push({user:result2[i].name, age: result2[i].fecha, more: { phone: result2[i].ubicacion } })
+      }
+      
+      let settings = {
+        fileName: "Listado Pasajeros Mina", // Name of the resulting spreadsheet
+        extraLength: 3, // A bigger number means that columns will be wider
+        writeMode: "writeFile", // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
+        writeOptions: {}, // Style options from https://docs.sheetjs.com/docs/api/write-options
+        RTL: false, // Display the columns from right-to-left (the default value is false)
+      }
+      
+      xlsx(data, settings) // Wil
+     
+    
+  }
 
   //filtrar los datos
-  const results = !search ? users : users.filter((val)=>val.fecha.toLowerCase().includes(search.toLocaleLowerCase()))
+ 
 
   return (
     <>
@@ -44,7 +95,7 @@ export const MinaPage = () => {
     <div className="Tabla">
     <h2 >Lista de Pasajeros</h2>
     <input value={search} onChange={searcher} type='text' data-date-format="YYYY-MM-DD" placeholder='Buscar por fecha en formato yyyy-mm-dd' className='form-control mt-3'/>
-    <button type="button" className="btn btn-success mb-5 mt-5" id="boton-excel" onClick={convertirEx}>Convertir a Excel</button>
+    <button type="button" className="btn btn-success mb-5 mt-5" id="boton-excel" onClick={converted}>Convertir a Excel</button>
     <table className="table table-light table-striped table-bordered">
   <thead>
     <tr className="table-dark">
